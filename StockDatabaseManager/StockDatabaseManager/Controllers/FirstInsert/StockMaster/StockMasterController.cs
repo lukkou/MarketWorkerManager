e4fb8a272c40db14a2ab75e418cf17f3c45b2fd2
lukkou <naoya.tamura@nxtg.co.jp>
2017-06-19 19:01:21 +0900

    app.Settingの値を参照するよう設定

--- StockDatabaseManager/StockDatabaseManager/Controllers/FirstInsert/StockMaster/StockMasterController.cs
+++ StockDatabaseManager/StockDatabaseManager/Controllers/FirstInsert/StockMaster/StockMasterController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,6 +20,7 @@ namespace StockDatabaseManager.Controllers
 		public void ExcelDownload()
 		{
 			//保存先を作成
+			string saveDirectory = ConfigurationManager.AppSettings["key1"];
 		}
 	}
 }
