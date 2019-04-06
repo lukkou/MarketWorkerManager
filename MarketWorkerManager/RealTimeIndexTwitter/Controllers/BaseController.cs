4665550a556f6bd7e13d1aea375c23c88af644dd
naoya_tamura <naoya.tamura@nxtg.co.jp>
2019-04-06 18:37:55 +0900

    指標発表を事前にツイッター予測するためのソリューションを追加

---
+++ MarketWorkerManager/RealTimeIndexTwitter/Controllers/BaseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RealtimeIndexImporter.Context;
+
+namespace RealtimeIndexImporter.Controllers
+{
+	class BaseController : IDisposable
+	{
+		public LogicContext Logic { get; private set; }
+
+		public BaseController()
+		{
+			Logic = new LogicContext();
+		}
+
+		public void Dispose()
+		{
+			Logic.Dispose();
+		}
+
+		/// <summary>
+		/// ローカルマシンのタイムゾーンを取得
+		/// </summary>
+		/// <returns></returns>
+		internal TimeSpan GetMyTimeZone()
+		{
+			return TimeZoneInfo.Local.BaseUtcOffset;
+		}
+	}
+}
---
+++ MarketWorkerManager/RealTimeIndexTwitter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeIndexTwitter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Eをクリックすると止まります");
+			while (true)
+			{
+				if (Console.KeyAvailable)
+				{
+					string inputKey = Console.ReadKey().Key.ToString();
+					if (inputKey == "E")
+					{
+						return;
+					}
+				}
+				//Console.WriteLine("動き続けている！！");
+			}
+		}
+	}
+}
