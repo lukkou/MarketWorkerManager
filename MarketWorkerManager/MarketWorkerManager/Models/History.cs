71fa2d5ce4d2471f87fe2ec03e880e29dadb8eb8
naoya_tamura <naoya.tamura@nxtg.co.jp>
2019-08-19 17:50:14 +0900

    売買情報を保持するテーブルモデルを追加　使うかは知らない 将来使うはず・。・v

---
+++ MarketWorkerManager/MarketWorkerManager/Models/History.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MarketWorkerManager.Models
+{
+    class History
+    {
+        /// <summary>
+        /// 通貨のシンボル
+        /// </summary>
+        [Key]
+        [Column("Symbol", Order = 0)]
+        [Display(Name = "Symbol")]
+        public string Symbol { get; set;}
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public long OrderNo { get; set; }
+
+    }
+}
